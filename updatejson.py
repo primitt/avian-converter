1cda40d352bbb23696e5dc17a278e47096439652
primitt <prianshmittra@gmail.com>
2022-03-07 20:58:46 -0800

    remove API key

--- updatejson.py
+++ updatejson.py
@@ -6,7 +6,7 @@ import datetime
 e = datetime.datetime.now()
 while True:
   try:
-    api = requests.get("http://data.fixer.io/api/latest?access_key=ac697ae49feb83aee98629941b57a547").json()
+    api = requests.get("http://data.fixer.io/api/latest?access_key=).json()
     print("Successfully got API on " + e.strftime("%Y-%m-%d %H:%M:%S"))
     with open("rate.json", "w") as r:
       r.write(json.dumps(api))
